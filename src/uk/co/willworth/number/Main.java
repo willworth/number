7b3bd69cb4694f95e2886dce086689275c9b1e6a
Will Worth <will@willworth.co.uk>
2016-05-07 17:22:06 +0200

    second class file added

---
+++ src/uk/co/willworth/number/Main.java
@@ -0,0 +1,11 @@
+package uk.co.willworth.number;
+//This is a comment
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Welcome to the number project.");
+    System.out.println("We have a range of number games to play.");
+    System.out.println("If you would like to see the list of games, press 'g'.");
+    System.out.println("If you wish to exit, press 'X'.");
+    }
+}
---
+++ src/uk/co/willworth/number/Second.java
@@ -0,0 +1,11 @@
+package uk.co.willworth.number;
+
+/**
+ * Created by Will on 07-May-16.
+ */
+public class Second {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the second class!");
+    }
+}
