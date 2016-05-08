0270ca83f07565dc872c594914e5c2a6a3f4be1f
Will Worth <will@willworth.co.uk>
2016-05-08 09:09:23 +0200

    prime tester

---
+++ src/uk/co/willworth/number/IsPrime.java
@@ -0,0 +1,29 @@
+package uk.co.willworth.number;
+
+/**
+ *
+ * This will be a simple divisibility test
+ * for i in range 0, x
+ * if x/i = %0:
+*        print "not a prime!
+ *        exit
+ * else
+ *      print "it's a prime!"
+ *
+ *
+ *
+ * Do I actually want to code this to return a boolean?  Would that be a good test?
+ *
+ *
+ *
+ */
+public class IsPrime {
+//    int testNumber;
+//
+   +
+//
+//    void primeTest(){
+//        if()
+    }
+
+}
