bb9a3519c6901210d50e9b5f4cbc863d462ffa81
Will Worth <will@willworth.co.uk>
2016-05-09 18:08:11 +0200

    basic looping!

--- src/uk/co/willworth/number/Fibonacci.java
+++ src/uk/co/willworth/number/Fibonacci.java
@@ -7,7 +7,21 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main (String[] args){
         System.out.println("Welcome to the Fibonacci sequence producer.");
+        Scanner inputScanner = new Scanner(System.in);
         System.out.println("How many Fibonacci numbers would you like to print?");
+        int quant = inputScanner.nextInt();
+        System.out.println("You have requested the first " + quant + " numbers of the sequence.");
+        int count;
+        for (count = 0; count <= quant; count = count+1) {
+            System.out.println("the count is now " + count);
+           // int first = 0;
+           // int second = 1;
+        }
+
+
+
+
+
 
     }
 }
