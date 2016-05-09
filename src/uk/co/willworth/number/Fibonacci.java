c664f1ad9edaafb3e9d1e8b613fe368f329db949
Will Worth <will@willworth.co.uk>
2016-05-09 17:24:55 +0200

    basic talk

--- src/uk/co/willworth/number/Fibonacci.java
+++ src/uk/co/willworth/number/Fibonacci.java
@@ -1,10 +1,13 @@
 package uk.co.willworth.number;
 
+import java.util.Scanner;
 /**
  * Created by Will on 09-May-16.
  */
 public class Fibonacci {
     public static void main (String[] args){
+        System.out.println("Welcome to the Fibonacci sequence producer.");
+        System.out.println("How many Fibonacci numbers would you like to print?");
 
     }
 }
