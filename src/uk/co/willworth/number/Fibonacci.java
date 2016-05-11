8dab71814e701a5ef8534a0826847dde761487fa
Will Worth <will@willworth.co.uk>
2016-05-11 06:11:33 +0200

    working fib

--- src/uk/co/willworth/number/Fibonacci.java
+++ src/uk/co/willworth/number/Fibonacci.java
@@ -5,27 +5,36 @@ import java.util.Scanner;
  * Created by Will on 09-May-16.
  */
 public class Fibonacci {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         System.out.println("Welcome to the Fibonacci sequence producer.");
         Scanner inputScanner = new Scanner(System.in);
         System.out.println("How many Fibonacci numbers would you like to print?");
         int quant = inputScanner.nextInt();
-        System.out.println("You have requested the first " + quant + " numbers of the sequence.");
-        int count;
-        int first = 0;
-        int second = 1;
-        for (count = 0; count <= quant; count = count+1) {
-            System.out.println("the count is now " + count);
-            int x = first + second;
-            System.out.println("Fib number "+ count +" is " + x);
-            first = second;
-            second = x;
+        //Catch error input as this will only work for
+        // first 45 numbers of Fibonacci as I'm using int.
+
+
+        if (quant >= 45) {
+            System.out.println("Sorry! This dummy program only runs as high as the first");
+            System.out.println("45 integers of the fibonacci sequence.  Try again!");
+        } else {
+            System.out.println("You have requested the first " + quant + " numbers of the sequence.");
+            int count;
+
+
+            int a = 0;
+            int b = 1;
+            for (count = 1; count <= quant; count = count + 1) {
+                System.out.println("the count is now " + count);
+
+                int result = a + b;
+                System.out.println("Fib number " + count + " is " + result);
+                a = b;
+                b = result;
+
+            }
+
+
         }
-
-
-
-
-
-
     }
 }
